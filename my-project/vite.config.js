638bd88e393dadca3971196b28851d88195bbd10
pegopa22 <dashaataraan11@gmail.com>
2026-07-23 00:23:30 +0300

    importing tailwind

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindscss from  '@tailwindscss'
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
