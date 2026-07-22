08ae87e91b82a547cbd34fcbd869cb5aa0914ffc
pegopa22 <dashaataraan11@gmail.com>
2026-07-23 00:11:54 +0300

    expressig my love for rozi

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
+          <H1>Rozi is the best person in the world</H1>
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
